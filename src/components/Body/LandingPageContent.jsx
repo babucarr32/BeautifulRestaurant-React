ae8afe218ddb8c46f739d8ec8b5af259959529ad
babucarr32 <baboucarry@gmail.com>
2023-04-02 11:41:23 +0000

    Showed different burgers in list

--- src/components/Body/LandingPageContent.jsx
+++ src/components/Body/LandingPageContent.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import image from "../../assets/images/NewBurger.png";
+import bbqBurger from "../../assets/images/BBQBurger.png";
+import carolina from "../../assets/images/Corolina.png";
+import friedChickenBurger from "../../assets/images/friedChickenBurger.png";
 import "../../assets/css/LandingPage.css";
 
 function LandingPageContent() {
@@ -25,9 +28,9 @@ function LandingPageContent() {
                 <div className="burgerTypes">
                   <ul>
                     <li><img src={image} alt="" /></li>
-                    <li><img src={image} alt="" /></li>
-                    <li><img src={image} alt="" /></li>
-                    <li><img src={image} alt="" /></li>
+                    <li><img src={bbqBurger} alt="" /></li>
+                    <li><img src={carolina} alt="" /></li>
+                    <li><img src={friedChickenBurger} alt="" /></li>
                   </ul>
                 </div>
             </div>
