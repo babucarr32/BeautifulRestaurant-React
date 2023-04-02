33fe55f6a84ff903d0fe936198e12080c6f15a67
babucarr32 <baboucarry@gmail.com>
2023-04-02 11:57:33 +0000

    Styled the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import "../../assets/css/Header.css"
 
 function Header() {
   return (
     <div>
-        <h1>I am Header</h1>
+        <div className="header">
+          <ul>
+            <li><a href="">Better Food</a></li>
+            <li><a href="">Order Now</a></li>
+            <li><a href="">Location</a></li>
+            <li className="contact"><a href="">Contact Us</a></li>
+          </ul>
+        </div>
     </div>
   );
 }
