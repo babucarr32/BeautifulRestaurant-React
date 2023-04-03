498a12741040bff1dcbf7b309034939ffc93ab58
babucarr32 <baboucarry@gmail.com>
2023-04-03 22:02:07 +0000

    Displayed the data from the database to menu

--- src/components/Body/Menu/Menu.jsx
+++ src/components/Body/Menu/Menu.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import "../../../assets/css/Header.css";
+// import DISHES from "../../../data/dishes"
+import DISHES from "../../../data/dishes";
+import "../../../assets/css/Menu.css";
+
 
 function Menu() {
-  return (
-    <div>
+  const mappedData = DISHES.map((data)=>{
+    return (
+      <div>
         <div className="menuItems">
-          
+          <img className="menuBurger" src={data.path} alt="" srcset="" />
+          <p>{data.name}</p>
         </div>
     </div>
   );
+})
+return (
+  <>
+  <div className="gridConatiner">
+    {mappedData}
+  </div>
+  </>
+)
 }
 
 export default Menu;
--- src/data/dishes.js
+++ src/data/dishes.js
@@ -1,23 +1,24 @@
-DISHES = [
+const DISHES = [
     {
         id: 0,
         name: "BBQ Burger",
         path: "assets/images/BBQBurger.png"
     },
     {
-        id: 0,
-        name: "Carolina Burger",
-        path: "assets/images/Carolina.png"
+        id: 1,
+        name: "Corolina Burger",
+        path: "assets/images/Corolina.png"
     },
     {
-        id: 0,
+        id: 2,
         name: "Burger",
         path: "assets/images/NewBurger.png"
     },
     {
-        id: 0,
+        id: 3,
         name: "Fried Chicken Burger",
         path: "assets/images/friedChickenBurger.png"
     }
 
 ]
+export default DISHES
