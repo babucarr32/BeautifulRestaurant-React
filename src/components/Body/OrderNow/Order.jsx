1c6038fc9e1f666eed3f18b4da0bf44fbc746d4c
babucarr32 <baboucarry@gmail.com>
2023-04-04 06:40:59 +0000

    ABout to add items to local storage

--- src/components/Body/OrderNow/Order.jsx
+++ src/components/Body/OrderNow/Order.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import {React, useState, useEffect} from "react";
 import "../../../assets/css/Header.css"
 
 function Order() {
+  const [items, setItems] = useState({name:"name", surname:"Surname"});
+  localStorage.setItem('items', JSON.stringify(items));
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    if (items) {
+    setItems(items);
+    }
+  }, []);
+
   return (
     <div>
         <h1>I am Orders</h1>
