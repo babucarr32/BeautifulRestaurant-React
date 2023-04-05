4ca9d1ce07b9b82396dc075427e5b8c68024346c
babucarr32 <baboucarry@gmail.com>
2023-04-05 06:43:46 +0000

    Displayed the data in order page

--- src/components/Body/OrderNow/Order.jsx
+++ src/components/Body/OrderNow/Order.jsx
@@ -1,22 +1,28 @@
 import {React, useState, useEffect} from "react";
+import DISHES from "../../../data/dishes";
 import "../../../assets/css/Header.css"
 
 function Order() {
-  // const [items, setItems] = useState({name:"name", surname:"Surname"});
-  // localStorage.setItem('items', JSON.stringify(items));
+  let [itemMapped, setItemMapped] = useState(null);
   useEffect(() => {
     const items = JSON.parse(localStorage.getItem('items'));
     if (items) {
-    // setItems(items);
-    console.log("Order")
-    console.log(items)
+      let mappedItems = items.map((e)=>{
+        // Filtering the data
+        const filterDish = DISHES.filter((dish)=>dish.name == e.burgerName);
+        return (
+          <div>
+            {/* Adding the filtered data to path */}
+            <img src={filterDish[0].path} alt="" srcset="" />
+              <h1>{e.burgerName}</h1>
+          </div>
+        );
+      })
+      setItemMapped(mappedItems)
     }
   }, []);
-
   return (
-    <div>
-        <h1>I am Orders</h1>
-    </div>
+    <h1>{itemMapped}</h1>
   );
 }
 
