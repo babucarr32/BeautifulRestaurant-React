876aebe5d93e7529a6977a0625cc6f4130ebd24d
babucarr32 <baboucarry@gmail.com>
2023-04-07 05:52:28 +0000

    Displayed the Totalprice and ProductTotal

--- src/components/Body/Menu/OrderSummary.jsx
+++ src/components/Body/Menu/OrderSummary.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 export default function OrderSummary(props){
-    const productTotal =props.items.length
-    console.log(productTotal);
+    const productTotal =props.items.length;
+    let productQuantity = 0;
+    let productPrice = 0;
     props.items.map((prop)=>{
-        console.log(prop[0].price)
+        productPrice += prop[0].price;
+        console.log(productQuantity += prop[0].price);
     })
     return (
         <div className="checkOut">
@@ -12,8 +14,8 @@ export default function OrderSummary(props){
             <h1>Order Summary</h1>
             <ul>
                 <li><p>Product Total: <span>{productTotal/2}</span></p></li>
-                <li><p>Product Quantity: <span>65</span></p></li>
-                <li><p>Total Price: <span>$87,400</span></p></li>
+                <li><p>Product Quantity: <span>{productQuantity}</span></p></li>
+                <li><p>Total Price: <span>$ {productPrice/2}</span></p></li>
             </ul>
             <div className="buttonDiv">
                 <button type="submit">Checkout</button>
