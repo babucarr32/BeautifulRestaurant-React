b4d2cb06b211d333edf416013249549c42ae60d5
babucarr32 <baboucarry@gmail.com>
2023-04-08 21:49:04 +0000

    Desingnig the contact page changed the opacity of the contact bg to .5

--- src/components/Body/ContactUs/Contact.jsx
+++ src/components/Body/ContactUs/Contact.jsx
@@ -6,14 +6,15 @@ function Contact() {
     <div className="contactContainer">
       <div className="infoDiv">
         <ul>
-          <li><p><span>Phone:</span> 3626260</p></li>
-          <li><span>Email: </span><a href="mailto:bbabucarr32@gmail.com">bbabucarr32@gmail.com</a></li>
-          <li><p><span>Location:</span>The, Gambia </p></li>
+          <li><img src="assets\images\call.svg" alt="" /><p><span>Phone:</span> +220 3626260</p></li>
+          <li><img src="assets\images\mail.svg" alt="" /><span>Email: </span><a href="mailto:bbabucarr32@gmail.com">bbabucarr32@gmail.com</a></li>
+          <li><img src="assets\images\location.svg" alt="" /><p><span>Location:</span>The, Gambia. </p></li>
         </ul>
       </div>
       <div className="imgDiv">
         <img src="assets\images\undraw_personal_text_re_vqj3.svg" alt="" srcset="" />
         <div className="formDiv">
+          <p>Get In Touch With Us</p>
           <form action="">
             <input type="text" placeholder="First name"/> <br />
             <input type="text" placeholder="Last name"/> <br />
