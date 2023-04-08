fba9838b78b30998262fccf4774f3700a5fdfd22
babucarr32 <baboucarry@gmail.com>
2023-04-08 07:53:05 +0000

    Made the useffect to run infinite and made the add and reduce quantity respnsive

--- src/components/Body/OrderNow/Order.jsx
+++ src/components/Body/OrderNow/Order.jsx
@@ -5,27 +5,32 @@ import "../../../assets/css/Order.css"
 
 function Order() {
   let [LSStorageItems, LSStorageItemsUpdate] = useState("");
+  let [itemMapped, mappedItems] = useState(null);
+  let [quantityCount, quantityCountUpdate] = useState(1);
 
+  const addProduct = (e, op)=>{
+    op === "+" ? quantityCountUpdate(quantityCount += 1): quantityCountUpdate(quantityCount -= 1)
+    console.log(op)
+  };
   useEffect(() => {
     LSStorageItemsUpdate = (LSStorageItems = JSON.parse(localStorage.getItem('items')));
-    
     if (LSStorageItems) {
-      let mappedItems = LSStorageItems.map((e)=>{
-        console.log(e)
+        mappedItems (itemMapped = LSStorageItems.map((e)=>{
         const filterDish = DISHES.filter((dish)=>dish.name == e.burgerName);
-        // return (
-        //   <div className="row">
-        //     <div className="cell">
-        //       <img src={filterDish[0].path} alt="" />
-        //     <p>{e.burgerName}</p>
-        //     </div>
-        //     <p className="quantity"><span onClick={(e)=>addProduct(e, filterDish[0].id, filterDish[0].price)} id="minus">-</span> <span id="quantity">1</span> <span onClick={(e)=>addProduct(e, filterDish[0].id, filterDish[0].price)} id="add">+</span></p>
-        //     <p>{e.burgerPrice}</p>
-        //   </div>
-        // );
-      })
+        return (
+          <div className="row">
+            <div className="cell">
+              <img src={filterDish[0].path} alt="" />
+            <p>{e.burgerName}</p>
+            </div>
+            <p className="quantity"><span onClick={(e)=>addProduct(e, "-")} id="minus">-</span> <span id="quantity">{quantityCount}</span> <span onClick={(e)=>addProduct(e, "+")} id="add">+</span></p>
+            <p>{e.burgerPrice}</p>
+          </div>
+        );
+      }))
     }
-  }, []);
+  });
+
   return (
     <div className="orderContainer">
       <div className="table">
@@ -33,7 +38,7 @@ function Order() {
           <div className="tableHeader"><p>QUANTITY</p></div>
           <div className="tableHeader"><p>PRICE</p></div>
 
-        {/* {itemMapped} */}
+        {itemMapped}
       </div>
       {/* <OrderSummary items = {orderedItems} quantity ={quantity}/> */}
     </div>
