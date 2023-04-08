d245113b6eeb5f000e8141757497d52bbbc0119b
babucarr32 <baboucarry@gmail.com>
2023-04-08 07:16:40 +0000

    Made the price in the db whole number

--- src/components/Body/Menu/Menu.jsx
+++ src/components/Body/Menu/Menu.jsx
@@ -5,56 +5,21 @@ import "../../../assets/css/Menu.css";
 
 
 function Menu() {
-  
   // Get value it button clicked
-  const itemToSave = ["Babucarr", "Badjie"];
-  const [items, setItems] = useState([]);
-  let itemNew = [];
   let newItems = {}
-  // if(items){
-  //   localStorage.setItem('items', JSON.stringify(items));
-  // }
-
-  const AddItem = (event, id)=>{
-    const burgerPrice = event.target.parentElement.children[0].innerText;
-    const burgerName = event.target.parentElement.parentElement.children[0].innerText;
+  const AddItem = (event, id, burgerPrice, burgerName)=>{
     
     let getLStorage = JSON.parse(localStorage.getItem('items'));
     if(getLStorage === null){
       newItems = [{id, burgerName, burgerPrice}]
       localStorage.setItem('items', JSON.stringify(newItems));
-      // getLStorage = JSON.parse(localStorage.getItem('items'));
-      // itemNew.push(getLStorage);
-      // setItems(newItems);
-      // console.log(itemNew)
     }
     else{
       getLStorage = JSON.parse(localStorage.getItem('items'));
       newItems = {id, burgerName, burgerPrice}
       getLStorage.push(newItems);
-      // setItems(getLStorage);
       localStorage.setItem('items', JSON.stringify(getLStorage));
-      // console.log("else")
-      // console.log(getLStorage)
-
     }
-    // const newItems = {burgerName, burgerPrice}
-    // itemNew.push(JSON.parse(localStorage.getItem('items')));
-    // setItems(newItems)
-    // console.log(typeof getLStorage)
-    // if(itemNew[0] === null){
-    //   console.log(items)
-    //   // localStorage.setItem('items', JSON.stringify(items));
-    //   // console.log(items)
-    //   // localStorage.setItem('items', JSON.stringify(items));
-    // }
-    // else{
-    //   // console.log(itemNew)
-    //   // itemNew.push(newItems)
-    //   // console.log(itemNew);
-    //   // setItems(itemNew)
-    //   // localStorage.setItem('items', JSON.stringify(items));
-    // }
   }
   
   const mappedData = DISHES.map((data)=>{
@@ -66,7 +31,7 @@ function Menu() {
             <p id="burgerName">{data.name}</p>
           <div className="burgerPriceDiv">
             <p id="burgerPrice">${data.price}. <sup>00</sup> </p>
-            <button onClick={(e)=>AddItem(e, data.id)} className="addButton">Add +</button>
+            <button onClick={(e)=>AddItem(e, data.id, data.price, data.name)} className="addButton">Add +</button>
           </div>
         </div>
       </div>
@@ -75,9 +40,9 @@ function Menu() {
 })
   return (
     <>
-    <div className="gridConatiner">
-      {mappedData}
-    </div>
+      <div className="gridConatiner">
+        {mappedData}
+      </div>
     </>
   )
 }
--- src/components/Body/OrderNow/Order.jsx
+++ src/components/Body/OrderNow/Order.jsx
@@ -4,89 +4,26 @@ import DISHES from "../../../data/dishes";
 import "../../../assets/css/Order.css"
 
 function Order() {
+  let [LSStorageItems, LSStorageItemsUpdate] = useState("");
 
-  let [quantity, updateQuantity] = useState(0)
-  const addProduct = (e, index, price)=>{
-    
-    // Adding the price and updating the database if "+" is clicked 
-    const filterDish = items.filter((dish)=>dish.id == index);
-    try{
-      let burgerPriceInt = filterDish[0].burgerPrice;
-      burgerPriceInt = burgerPriceInt.split(".")[0].split("$")[1]
-      filterDish[0].burgerPrice = parseInt(burgerPriceInt) + price;
-      console.log(filterDish[0].burgerPrice);
-    }
-    catch(e){
-      let burgerPriceInt = filterDish[0].burgerPrice;
-      filterDish[0].burgerPrice = parseInt(burgerPriceInt) + price;
-      console.log(filterDish[0].burgerPrice);
-    }
-
-    const OP = e.target.textContent;
-    // console.log(e.target.parentElement.children[1].textContent);
-    const quantitySpplitter = e.target.parentElement.textContent.split(" ");
-    // Incrementing number
-    if(OP === "+"){
-      try{
-        updateQuantity(mappedItems.length += 1);
-      }
-      catch(e){
-        return e
-      }
-      // console.log(quantity);
-      let innerValue = e.target.parentElement.children[1].textContent;
-      // Getting the burgerPrice
-      let burgerPrice = e.target.parentElement.parentElement.children[2].textContent.split(".")[0].split("$")[1];
-      console.log(orderedItems);
-      // setOrderedItems
-      e.target.parentElement.children[1].textContent = parseInt(innerValue) + 1 ;
-      // console.log(quantity);
-
-    }
-    else{
-      try{
-        updateQuantity(mappedItems.length -= 1);
-      }
-      catch(e){
-        return e
-      }
-      let innerValue = e.target.parentElement.children[1].textContent;
-      if(innerValue > 1){
-        e.target.parentElement.children[1].textContent = parseInt(innerValue) - 1 ;
-      }
-    }
-  }
-  let [itemMapped, setItemMapped] = useState(null);
-  let [orderedItems, setOrderedItems] = useState([]);
-  let [items, setItems] = useState("");
-  let addItems = [];
-  let mappedItems = [];
-  // let items = ""; 
   useEffect(() => {
-    setItems(items = JSON.parse(localStorage.getItem('items')));
-
-    if (items) {
-      mappedItems = items.map((e)=>{
-        // Filtering the data
+    LSStorageItemsUpdate = (LSStorageItems = JSON.parse(localStorage.getItem('items')));
+    
+    if (LSStorageItems) {
+      let mappedItems = LSStorageItems.map((e)=>{
+        console.log(e)
         const filterDish = DISHES.filter((dish)=>dish.name == e.burgerName);
-        console.log(filterDish)
-        setItemMapped(orderedItems.push(filterDish));
-        return (
-          <div className="row">
-            {/* Adding the filtered data to path */}
-            <div className="cell">
-              <img src={filterDish[0].path} alt="" />
-            <p>{e.burgerName}</p>
-            </div>
-            <p className="quantity"><span onClick={(e)=>addProduct(e, filterDish[0].id, filterDish[0].price)} id="minus">-</span> <span id="quantity">1</span> <span onClick={(e)=>addProduct(e, filterDish[0].id, filterDish[0].price)} id="add">+</span></p>
-            <p>{e.burgerPrice}</p>
-          </div>
-        );
+        // return (
+        //   <div className="row">
+        //     <div className="cell">
+        //       <img src={filterDish[0].path} alt="" />
+        //     <p>{e.burgerName}</p>
+        //     </div>
+        //     <p className="quantity"><span onClick={(e)=>addProduct(e, filterDish[0].id, filterDish[0].price)} id="minus">-</span> <span id="quantity">1</span> <span onClick={(e)=>addProduct(e, filterDish[0].id, filterDish[0].price)} id="add">+</span></p>
+        //     <p>{e.burgerPrice}</p>
+        //   </div>
+        // );
       })
-      setItemMapped(mappedItems);
-      // console.log(quantity);
-      updateQuantity(mappedItems.length);
-      console.log(quantity);
     }
   }, []);
   return (
@@ -96,9 +33,9 @@ function Order() {
           <div className="tableHeader"><p>QUANTITY</p></div>
           <div className="tableHeader"><p>PRICE</p></div>
 
-        {itemMapped}
+        {/* {itemMapped} */}
       </div>
-      <OrderSummary items = {orderedItems} quantity ={quantity}/>
+      {/* <OrderSummary items = {orderedItems} quantity ={quantity}/> */}
     </div>
   );
 }
