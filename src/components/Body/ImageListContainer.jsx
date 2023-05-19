3a87ac211862d7143b2552f81b41250da5a5e0ba
babucarr32 <baboucarry@gmail.com>
2023-05-19 15:41:04 +0000

    using create context and usecontext

---
+++ src/components/Body/ImageListContainer.jsx
@@ -0,0 +1,45 @@
+import React, { useContext } from "react";
+import List from "./List";
+import BurgerImage from "./BurgerImage";
+import { BurgerTypeContext } from "./WelcomeContainer";
+
+function ImageListContainer() {
+  const {
+    image,
+    bbqBurger,
+    carolina,
+    friedChickenBurger,
+    changeImageOnClick,
+    changeImage,
+  } = useContext(BurgerTypeContext);
+
+  return (
+    <div className="burgerTypes">
+      <ul>
+        <List
+          onClickFunc={() => changeImage(image)}
+          child={<BurgerImage imagePath={image} imgName={"image"} />}
+        />
+        <List
+          onClickFunc={() => changeImage(bbqBurger)}
+          child={<BurgerImage imagePath={bbqBurger} imgName={"NewBurger"} />}
+        />
+        <List
+          onClickFunc={() => changeImage(carolina)}
+          child={<BurgerImage imagePath={carolina} imgName={"carolina"} />}
+        />
+        <List
+          onClickFunc={() => changeImage(friedChickenBurger)}
+          child={
+            <BurgerImage
+              imagePath={friedChickenBurger}
+              imgName={"friedChickenBurger"}
+            />
+          }
+        />
+      </ul>
+    </div>
+  );
+}
+
+export default ImageListContainer;
--- src/components/Body/WelcomeContainer.jsx
+++ src/components/Body/WelcomeContainer.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, createContext } from "react";
 import BurgerImage from "./BurgerImage";
 import SocialMediaSites from "./SocialMediaSites";
 import WelcomeText from "./WelcomeText";
 import WelcomeIntro from "./WelcomeIntro";
 import List from "./List";
+import ImageListContainer from "./ImageListContainer";
+
+export const BurgerTypeContext = createContext(null);
 
 function WelcomeContainer() {
   const [image, setImage] = useState("/assets/images/NewBurger.png");
@@ -21,64 +24,47 @@ function WelcomeContainer() {
   };
 
   return (
-    <div className="welcomeText">
-      <WelcomeText />
-      <WelcomeIntro />
+    <BurgerTypeContext.Provider
+      value={{
+        image,
+        bbqBurger,
+        carolina,
+        friedChickenBurger,
+        changeImageOnClick,
+        changeImage,
+      }}
+    >
+      <div className="welcomeText">
+        <WelcomeText />
+        <WelcomeIntro />
 
-      <ul>
-        <List className={"Mild"} spice={"Mild"} />
-        <List className={"Spicy"} spice={"MilSpicyd"} />
-        <List className={"Hot"} spice={"Hot"} />
-      </ul>
-      <div className="addToCart">
         <ul>
-          <List className={"addToCartText"} spice={"Add +"} />
-          <List className={"price"} spice={"$12.99"} />
-          <li className="delList">
-            <del>$16.99</del>
-          </li>
+          <List className={"Mild"} spice={"Mild"} />
+          <List className={"Spicy"} spice={"MilSpicyd"} />
+          <List className={"Hot"} spice={"Hot"} />
         </ul>
+        <div className="addToCart">
+          <ul>
+            <List className={"addToCartText"} spice={"Add +"} />
+            <List className={"price"} spice={"$12.99"} />
+            <li className="delList">
+              <del>$16.99</del>
+            </li>
+          </ul>
+        </div>
+        <div className="hideBurgerFirst">
+          <BurgerImage
+            id={"hideBurgerFirst"}
+            imageRef={changeMainImageRef}
+            imagePath={image}
+          />
+        </div>
+
+        <ImageListContainer />
+
+        <SocialMediaSites />
       </div>
-      <div className="hideBurgerFirst">
-        <BurgerImage
-          id={"hideBurgerFirst"}
-          imageRef={changeMainImageRef}
-          imagePath={image}
-        />
-      </div>
-      <div className="burgerTypes">
-        <ul>
-          <List
-            onClickFunc={() => changeImage(image)}
-            child={
-              <BurgerImage
-                imagePath={image}
-                imgClass={"mainImages"}
-                imgName={"image"}
-              />
-            }
-          />
-          <List
-            onClickFunc={() => changeImage(bbqBurger)}
-            child={<BurgerImage imagePath={bbqBurger} imgName={"NewBurger"} />}
-          />
-          <List
-            onClickFunc={() => changeImage(carolina)}
-            child={<BurgerImage imagePath={carolina} imgName={"carolina"} />}
-          />
-          <List
-            onClickFunc={() => changeImage(friedChickenBurger)}
-            child={
-              <BurgerImage
-                imagePath={friedChickenBurger}
-                imgName={"friedChickenBurger"}
-              />
-            }
-          />
-        </ul>
-      </div>
-      <SocialMediaSites />
-    </div>
+    </BurgerTypeContext.Provider>
   );
 }
 
