89892f4f345d739d5784994b23cacf907b6b8404
babucarr32 <baboucarry@gmail.com>
2023-05-19 13:21:44 +0000

    Moved Welcome to its own component

--- src/components/Body/LandingPageContent.jsx
+++ src/components/Body/LandingPageContent.jsx
@@ -1,28 +1,10 @@
-import React, { useState } from "react";
-import image from "../../assets/images/NewBurger.png";
+import React from "react";
+import "../../assets/css/LandingPage.css";
+import WelcomeContainer from "./WelcomeContainer";
 import BurgerImage from "./BurgerImage";
 import SocialMediaSites from "./SocialMediaSites";
-import "../../assets/css/LandingPage.css";
-import bbqBurger from "../../assets/images/NewBurger.png";
-import carolina from "../../assets/images/Corolina.png";
-import friedChickenBurger from "../../assets/images/friedChickenBurger.png";
 
 const LandingPageContent = () => {
-  const [image, setImage] = useState("/assets/images/NewBurger.png");
-  const [bbqBurger, setBbqBurger] = useState("/assets/images/NewBurger.png");
-  const [carolina, setCarolina] = useState("/assets/images/Corolina.png");
-  const [friedChickenBurger, setFriedChickenBurger] = useState(
-    "/assets/images/friedChickenBurger.png"
-  );
-
-  const changeImage = (event) => {
-    const imagePath = event.target.name;
-    console.log(imagePath);
-    try {
-      document.getElementById("hideBurgerLast").src = this.state[imagePath];
-      document.getElementById("hideBurgerFirst").src = this.state[imagePath];
-    } catch (err) {}
-  };
   return (
     <div>
       <div className="landingContent">
@@ -30,66 +12,9 @@ const LandingPageContent = () => {
           <div className="hideBurgerLast">
             <SocialMediaSites />
           </div>
-          <div className="welcomeText">
-            <p className="welcome">
-              <span>Welcome</span> to <br /> The worlds <br />{" "}
-              <span>Best Restaurant</span>
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.{" "}
-            </p>
-            <ul>
-              <li>
-                <p className="Mild">Mild</p>
-              </li>
-              <li>
-                <p className="Spicy">Spicy</p>
-              </li>
-              <li>
-                <p className="Hot">Hot</p>
-              </li>
-            </ul>
-            <div className="addToCart">
-              <ul>
-                <li>
-                  <p className="addToCartText">Add +</p>
-                </li>
-                <li>
-                  <p className="price">$12.99</p>
-                </li>
-                <li className="delList">
-                  <del>$16.99</del>
-                </li>
-              </ul>
-            </div>
-            <div className="hideBurgerFirst">
-              <BurgerImage id={"hideBurgerFirst"} />
-            </div>
-            <div className="burgerTypes">
-              <ul>
-                <li onClick={changeImage}>
-                  <img className="mainImages" src={image} name="image" alt="" />
-                </li>
-                <li onClick={changeImage}>
-                  <img src={bbqBurger} alt="" name="bbqBurger" />
-                </li>
-                <li onClick={changeImage}>
-                  <img src={carolina} alt="" name="carolina" />
-                </li>
-                <li onClick={changeImage}>
-                  <img
-                    src={friedChickenBurger}
-                    alt=""
-                    name="friedChickenBurger"
-                  />
-                </li>
-              </ul>
-            </div>
-            <SocialMediaSites />
-          </div>
+
+          <WelcomeContainer />
+
           <div className="hideBurgerLast">
             <BurgerImage id={"hideBurgerLast"} />
           </div>
