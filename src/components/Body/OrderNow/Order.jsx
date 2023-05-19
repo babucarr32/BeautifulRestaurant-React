8f8c968421be1a1257d0f0ea8dec17ac7ab4d302
babucarr32 <baboucarry@gmail.com>
2023-05-19 23:42:48 +0000

    Made the images and prices dynamic

--- src/components/Body/OrderNow/Order.jsx
+++ src/components/Body/OrderNow/Order.jsx
@@ -4,6 +4,33 @@ import DISHES from "../../../data/dishes";
 import "../../../assets/css/Order.css";
 
 function Order() {
+  const [lsItems, setLsItems] = useState([]);
+  useEffect(() => {
+    const fetchItemsFromLS = () => {
+      const items = localStorage.getItem("items");
+      if (items) setLsItems(JSON.parse(items));
+    };
+    fetchItemsFromLS();
+  }, []);
+
+  const itemsJSX = lsItems.map((items) => {
+    const imageFindter = DISHES.find((dish) => dish.id === items.id);
+    return (
+      <tr>
+        <td>
+          <img src={imageFindter.path} alt="" srcset="" />
+        </td>
+        <td>
+          <span>+</span>
+          <p>10</p> <span>-</span>
+        </td>
+        <td>
+          <p>${items.burgerPrice}</p>
+        </td>
+      </tr>
+    );
+  });
+
   return (
     <div className="orderContainer">
       <table>
@@ -12,37 +39,9 @@ function Order() {
           <th>QUANTITY</th>
           <th>PRICE</th>
         </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
+        {itemsJSX}
       </table>
+
       {/* <OrderSummary items={orderedItems} /> */}
     </div>
   );
